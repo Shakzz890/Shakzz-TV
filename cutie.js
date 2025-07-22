485fc418281c1fbae01727ecf282fdc95bb61628
Shakzz890 <120597222+Shakzz890@users.noreply.github.com>
2025-07-22 16:48:26 +0800

    Update cutie.js

--- cutie.js
+++ cutie.js
@@ -945,6 +945,8 @@ dreamworks_tagalized: {
     logo: "https://yt3.googleusercontent.com/oT4LFcdusmWboxy8ZC9c6NS0riqRi6_96U-gXLT7C-NarXKKFjLDbyko6iFJVfr743e4eTnv=s900-c-k-c0x00ffffff-no-rj",
 };
 
+ };
+ 
 let currentChannelKey = "kapamilya"; // Default channel key
 
 function renderChannelButtons(filter = "") {
