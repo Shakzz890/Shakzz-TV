d211af18aa30142f146594de0bf996ad5dc16bf0
Shakzz <120597222+Shakzz890@users.noreply.github.com>
2026-08-30 17:43:39 +0800

    Update cutie.js

--- cutie.js
+++ cutie.js
@@ -1419,28 +1419,25 @@ async function handleAdminLogin() {
             if (inputHash === storedHash) {
                 if(els.nicknameInput) els.nicknameInput.value = ADMIN_NICKNAME;
 
-                currentUser = {
-                    uid: "master_admin_shakzz"
+                localStorage.setItem("chat_user", JSON.stringify({
+                    uid: currentUser.uid,
+                    nickname: ADMIN_NICKNAME,
+                    avatarUrl: SHAKZZ_ADMIN_AVATAR,
+                    isAdmin: true
+                }));
+
+                await db.ref(`users/${currentUser.uid}`).update({
+                    uid: currentUser.uid,
+                    nickname: ADMIN_NICKNAME,
+                    avatarUrl: SHAKZZ_ADMIN_AVATAR,
+                    isAdmin: true
+                });
+
+                cachedUserData = {
+                    nickname: ADMIN_NICKNAME,
+                    avatarUrl: SHAKZZ_ADMIN_AVATAR,
+                    isAdmin: true
                 };
-
-               localStorage.setItem("chat_user", JSON.stringify({
-    uid: currentUser.uid, 
-    nickname: ADMIN_NICKNAME,
-    avatarUrl: SHAKZZ_ADMIN_AVATAR,
-    isAdmin: true
-}));
-
-await db.ref(`users/${currentUser.uid}`).update({
-    nickname: ADMIN_NICKNAME,
-    avatarUrl: SHAKZZ_ADMIN_AVATAR,
-    isAdmin: true
-});
-
-cachedUserData = {
-    nickname: ADMIN_NICKNAME,
-    avatarUrl: SHAKZZ_ADMIN_AVATAR,
-    isAdmin: true
-};
 
                 showAdminWelcome();
                 showChat();
